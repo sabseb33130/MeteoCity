66615db8d7f02704b3b5fa8ba1ab5b27f4d5f44a
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-11-22 20:46:00 +0100

    test de la method fetch

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+    
+
 const openID = "f3b93101c2e5243ad953a1d5d8af1da4";
 /*const villeChoix = document.getElementById("ville");
 let choix = villeChoix.ariaValueText;
@@ -6,15 +8,22 @@ console.log(choix);*/
 function geoVille(){
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Toulouse,FR&limit=5&appid=${openID}`)
     .then((resp)=> resp.json())
-    .then((data) => console.log(data))
+    .then((data) => console.log(JSON.stringify(data)))
      
 };
-//const lat = resp.json().lat ;
+
 geoVille();
 function weathVille(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=1.5032717763608257&appid=${openID}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=43.6044622&lon=1.5032717763608257&appid=${openID}`)
     .then((resp1)=> resp1.json())
-    .then((data1) => console.log(data1))   
+    .then((data1) => console.log((data1))) 
+    .then((data1) => console.log((data1.main.temp)))
+    .then((data1) => console.log((data1.main.humidity)))
+    .then((data1) => console.log((data1.main.pressure)))
+    .catch((data1) =>console.log("erreur"))
+let test = data1.main.humidity;
+console.log(test);
 };
 weathVille();
+
 
